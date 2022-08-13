d3854e090f9277349274498f2e04fa65133c0658
me-cafebabe <me_cafebabe@hotmail.com>
2022-08-14 02:49:39 +0800

    msm8916-common: Import configurations from Vincent's msm8916-common
    Co-authored-by: Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -5,3 +5,57 @@
 #
 
 COMMON_PATH := device/samsung/msm8916-common
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a53
+
+# Binder
+TARGET_USES_64_BIT_BINDER := true
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := MSM8916
+
+# Kernel
+BOARD_KERNEL_CMDLINE += \
+	console=null \
+	androidboot.hardware=qcom \
+	user_debug=23 \
+	msm_rtb.filter=0x3F \
+	ehci-hcd.park=3 \
+	androidboot.bootdevice=7824900.sdhci \
+	androidboot.selinux=permissive
+
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_SEPARATED_DT := true
+BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
+BOARD_RAMDISK_OFFSET := 0x02000000
+
+# Partition sizes
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 15728640
+
+# Ramdisk
+BOARD_RAMDISK_USE_LZMA := true
+LZMA_RAMDISK_TARGETS := recovery
+
+# Recovery
+TARGET_RECOVERY_PIXEL_FORMAT := RGB_565
+
+# TWRP
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_HAS_DOWNLOAD_MODE := true
+TW_HAS_MTP := true
+#TW_INPUT_BLACKLIST := "accelerometer\x0ahbtp_vm"
+TW_INTERNAL_STORAGE_PATH := "/data/media/0"
+TW_MAX_BRIGHTNESS := 255
+TW_MTP_DEVICE := /dev/mtp_usb
+TW_NO_REBOOT_BOOTLOADER := true
+TW_NO_USB_STORAGE := true
+TW_THEME := portrait_hdpi
