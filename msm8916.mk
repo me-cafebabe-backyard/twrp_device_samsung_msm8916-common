42a970244f4da0c197694cf31a7f36bcdb19d585
me-cafebabe <me_cafebabe@hotmail.com>
2022-08-14 02:52:20 +0800

    msm8916-common: We don't use usb configfs

--- msm8916.mk
+++ msm8916.mk
@@ -7,6 +7,9 @@
 # Inherit AOSP product makefiles
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
+# USB
+PRODUCT_PROPERTY_OVERRIDES += sys.usb.configfs=0
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
